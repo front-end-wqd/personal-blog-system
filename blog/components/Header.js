46bf1a55148dc4080276d2743534629aca36ed5e
wqd182212 <10104911+wqd182212@user.noreply.gitee.com>
2021-12-20 18:04:29 +0800

    通用部分代码编写

--- blog/components/Header.js
+++ blog/components/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import styles from '../styles/components/Header.module.scss';
 import { Row, Col, Menu } from 'antd';
-import Icon from '@ant-design/icons';
+import { TrophyTwoTone, CodeTwoTone } from '@ant-design/icons';
 
 const Header = () => {
     return (
         <div className={styles.header}>
             <Row>
+                <Col flex="auto" className={styles["header-name"]}>
+                    <TrophyTwoTone style={{fontSize: '1.5em'}} twoToneColor="#1890ff" />
+                    <span className={styles["header-name-text"]}>前端开发吴彦祖</span>
+                </Col>
                 <Col>
-                    <span>前端开发吴彦祖</span>
+                    <Menu mode="horizontal">
+                        <Menu.Item key="css" icon={<CodeTwoTone twoToneColor="#1890ff" />}>
+                        你不知道的css
+                        </Menu.Item>
+                        <Menu.Item key="js" icon={<CodeTwoTone twoToneColor="#1890ff" />}>
+                        你不知道的js
+                        </Menu.Item>
+                    </Menu>
                 </Col>
             </Row>
         </div>
--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,7 +1,53 @@
+import React, { useState } from 'react';
 import Head from 'next/head'
 import Header from '../components/Header';
+import { Row, Col, List } from 'antd';
+import { CalendarOutlined, TagOutlined, UserOutlined } from '@ant-design/icons';
 
 export default function Home() {
+  const [list, setList] = useState([
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+    {
+      title: '你不知道的css',
+      description: '你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的你不知道的'
+    },
+  ]);
+
   return (
     <div>
       <Head>
@@ -9,9 +55,27 @@ export default function Home() {
         <meta name="description" content="blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <Header />
-      </div>
+      <Header />
+      <Row className="main">
+        <Col className="main-left">
+          <List
+            header={<div style={{fontWeight: 500, fontSize: 'calc(1em + 1em / 8)'}}>最新文章</div>}
+            dataSource={list}
+            renderItem={item => (
+              <List.Item>
+                <div className="article-title">{item.title}</div>
+                <div className="article-information">
+                  <span><CalendarOutlined style={{marginRight: 2}} />2021-12-20</span>
+                  <span><TagOutlined style={{marginRight: 2}} />css</span>
+                  <span><UserOutlined style={{marginRight: 2}} />123</span>
+                </div>
+                <div>{item.description}</div>
+              </List.Item>
+            )} />
+        </Col>
+        <Col className="main-right">
+        </Col>
+      </Row>
     </div>
   )
 }
